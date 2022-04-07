a4cbf6c15d0440f3ef69d863bd2a2c7a23bb183d
Tomas Chirieleison <tomaschirieleison2015@gmail.com>
2022-04-07 20:18:32 -0300

    save1

--- Pre_entrega1/urls.py
+++ Pre_entrega1/urls.py
@@ -18,7 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppRegistro/', include('AppRegistro.urls')),
+    path('pedidos/', include('AppRegistro.urls'))
 ]
